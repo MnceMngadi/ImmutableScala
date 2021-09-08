b9014c6badb55fa0a0fe2533fbd3a3624ee042af
Mncedisi <mncehelpfull@gmail.com>
2021-09-08 13:01:19 +0200

    Showing immutable values in Scala.

---
+++ src/Immutable.scala
@@ -0,0 +1,16 @@
+object Immutable {
+
+  def main(args: Array[String]): Unit = {
+
+    val names = Array ("Mncedisi","Mngadi","Ngema")
+
+
+    for(x <- names){
+
+      println(x)
+
+    }
+
+  }
+
+}
